f94516b5102c0c0d93adbdb9a98e826e43aba0cb
Jarrett Croll <jarrett.croll@gmail.com>
2013-09-10 16:15:18 -0700

    item box shadow hover effect working

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(".v-product-tile").hover(
+    $(".v-product-tile .thumbnail").hover(
         function(){ $(this).addClass("box-shadow") },
         function(){ $(this).removeClass("box-shadow") }
     );
